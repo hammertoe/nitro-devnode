e1992211da45d2c6bc5d5a7e1b0008356491df82
Joshua Colvin <jcolvin@offchainlabs.com>
2024-11-02 18:24:35 -0700

    Enable debug API calls
    `cargo stylus replay` requires `debug_traceTransaction`

--- run-dev-node.sh
+++ run-dev-node.sh
@@ -2,7 +2,7 @@
 
 # Start Nitro dev node in the background
 echo "Starting Nitro dev node..."
-docker run --rm --name nitro-dev -p 8547:8547 offchainlabs/nitro-node:v3.2.1-d81324d --dev --http.addr 0.0.0.0 &
+docker run --rm --name nitro-dev -p 8547:8547 offchainlabs/nitro-node:v3.2.1-d81324d --dev --http.addr 0.0.0.0 --http.api=net,web3,eth,debug &
 
 # Wait for the node to initialize
 echo "Waiting for the Nitro node to initialize..."
